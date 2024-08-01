c3bbed2255e62833224226ec07825b653c7df09e
MiguelEstanga12 <miguelestanga12@gmail.com>
2024-08-01 16:20:13 -0400

    prgreso login

--- Resources/views/activacion/index.blade.php
+++ Resources/views/activacion/index.blade.php
@@ -30,57 +30,68 @@
                 @if ($numero !== null)
                     <div class="container-fluid">
                         <div class="">
-
-                            <p id="bd_codigo" style="display: none;">
-                                codigo
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#145a32"
+                            <ul style="margin-bottom: 30px;">
+                                <li >
+                                    <div class="cntainer" id="estado_ws_container">
+                                        @if ($data_companias->estado_ws === 0)
+                                            Estado: <span id="estado">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#5f6a6a"
+                                                    class="bi bi-lightbulb-off-fill" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M2 6c0-.572.08-1.125.23-1.65l8.558 8.559A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m10.303 4.181L3.818 1.697a6 6 0 0 1 8.484 8.484zM5 14.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5M2.354 1.646a.5.5 0 1 0-.708.708l12 12a.5.5 0 0 0 .708-.708z" />
+                                                </svg>
+                                            </span>
+                                        @elseif($data_companias->estado_ws == 1)
+                                            Estado:
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f1c40f" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
+                                                <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/>
+                                            </svg>
+                                        @elseif($data_companias->estado_ws == 2)
+                                            Estado:
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#145a32" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
+                                                <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/>
+                                              </svg>
+                                           
+                                        @endif
+                                    </div>
+                                </li>
+                                <li style="display: none;" id="codigobd">
+                                    Codigo en linea: 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#145a32"
                                     class="bi bi-check" viewBox="0 0 16 16">
                                     <path
                                         d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
-                                </svg>
-                            </p>
-                            <p id="bd_verificado" style="display: none;">verificado
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#145a32"
+                                    </svg>
+                                </li>
+                                <li style="display: none;" id="codigows">
+                                    Codigo de ws:
+                                   
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#145a32"
                                     class="bi bi-check" viewBox="0 0 16 16">
                                     <path
                                         d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
-                                </svg>
-                            </p>
-
+                                    </svg>
+                                </li>
+                                <li  id="code_ws_container">
+                                    Introduce el codigo en tu ws:
+                                     <span id="code"> </span> 
+                                </li>
+                                <li style="display: none;" id="loginfinalizado">
+                                    Login finalizado:
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#145a32"
+                                    class="bi bi-check" viewBox="0 0 16 16">
+                                    <path
+                                        d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
+                                    </svg>
+                                </li>
+                            </ul>
+                            
+                           
+                          
                         </div>
-                        <div class="cntainer" id="estado_ws_container">
-                            @if ($data_companias->estado_ws === null)
-                                Estado: <span id="estado">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#5f6a6a"
-                                        class="bi bi-lightbulb-off-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M2 6c0-.572.08-1.125.23-1.65l8.558 8.559A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m10.303 4.181L3.818 1.697a6 6 0 0 1 8.484 8.484zM5 14.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5M2.354 1.646a.5.5 0 1 0-.708.708l12 12a.5.5 0 0 0 .708-.708z" />
-                                    </svg>
-                                </span>
-                            @elseif($data_companias->estado_ws === 1)
-                                Estado: <span id="estado">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold"
-                                        class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
-                                        <path
-                                            d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5" />
-                                    </svg>
-                                </span>
-                            @elseif($data_companias->estado_ws === 2)
-                                Estado: <span id="estado" style="color:gold;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" color="gold" width="20" height="20"
-                                        fill="#fff" class="bi bi-check" viewBox="0 0 16 16">
-                                        <path
-                                            d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
-                                    </svg>
-                                </span>
-                            @endif
-                        </div>
+                        
                     </div>
-                    <div class="container" style="margin: 10px">
-                        <div class="container alert alert-success " style="width:90vw!important;" id="code_ws_container">
-                            code: <span id="code"> {{ $data_companias->code_ws ?? '' }} </span>
-                        </div>
-                    </div>
+                   
                     <!--div class="container" id="code_container" style="margin: 10px">
                                             <div class="cntainer" >
 
@@ -125,26 +136,37 @@
                 fetch(`{{ route('lubot.default_compania') }}`)
                     .then(response => response.json())
                     .then(data => {
+                        console.log(data)
                         // Verificar si los datos están definidos y no están vacíos
-                        if (data.code_ws != null) seguir = bd_codigo.style.display = 'flex';
-                        if (data.estado_ws != null) seguir = bd_verificado.style.display = 'flex';
-
-                        if (data.estado_ws === 1 && data.code_ws != null) {
-                            document.getElementById('code').innerHTML = data.code_ws;
-                            document.getElementById('iniciar').display = 'none';
-                            document.getElementById('code_ws_container').style.display = 'flex';
-                            document.getElementById('estado').innerHTML = `
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
-                                <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/>
-                            </svg>`
+                        if (data.code_ws != null)  codigobd.style.display = 'block';
+                        if (data.estado_ws === 1 || data.estado_ws === 2 )   codigows.style.display = 'block';
+                        if (data.estado_ws === 2 )   {
+                             
                             document.getElementById('iniciar').style.display = 'none';
+                            loginfinalizado.style.display = 'block'
+                        }
 
+                        if( (data.estado_ws === 1 || data.estado_ws === 2) && data.code_ws != null ){
+                            code_ws_container.style.display = "block"
+                            document.getElementById('code').innerHTML = data?.code_ws ?? '';
+                            document.getElementById('estado').innerHTML = `
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f1c40f" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
+                                    <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/>
+                                </svg>
+                            `
+                        } 
+
+                        if (  data.estado_ws === 2 ) 
+                        {
+                           
+                            document.getElementById('estado').innerHTML = `
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f1c40f" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
+                                                <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"/>
+                                            </svg>`
                             clearInterval(intervalo); // Detener la cuenta regresiva
                             document.getElementById('loader').style.display = 'none'
                             clearInterval(pollingInterval); // Detener el polling
-                        } else {
-                            document.getElementById('code_ws_container').style.display = 'none';
-                        }
+                        } 
                     })
                     .catch(error => {
                         console.error('Error:', error);
@@ -176,7 +198,7 @@
                 start = true;
                 document.getElementById('iniciar').innerText = "Procesando...";
 
-                
+                /*
                 fetch(`{{ route('probarbot') }}`)
                     .then(response => response.json())
                     .then(data => {
@@ -190,7 +212,7 @@
                     .finally(function () {
                         alert('El bot acaba de iniciar, debe esperar alrededor de 60 segundos');
                     });
-               
+               **/
                 cuentaRegresiva(120);
 
                 // Iniciar polling para verificar el estado_ws
